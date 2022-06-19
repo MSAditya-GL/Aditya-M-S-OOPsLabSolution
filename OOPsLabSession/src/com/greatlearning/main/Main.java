bc14aa106bd9feac8567f50080e1f31f7ed8af43
Aditya <adigowdtham@gmail.com>
2022-06-19 19:15:30 +0530

    Updated file

---
+++ OOPsLabSession/src/com/greatlearning/main/Main.java
@@ -0,0 +1,57 @@
+package com.greatlearning.main;
+
+import java.util.Scanner;
+
+import com.greatlearning.module.Employee;
+
+import com.greatlearning.services.CredentialService;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		Employee emp1 = new Employee("Aditya", "muthusamy");
+		CredentialService cs = new CredentialService();
+
+		System.out.println("Please enter the department from the following ");
+		System.out.println("1. Technical");
+		System.out.println("2. Admin");
+		System.out.println("3. Human Resource");
+		System.out.println("4. Legal");
+
+		int option = in.nextInt();
+		if (option > 0) {
+			switch (option) {
+			case 1: {
+				String email = cs.generateEmailAddress(emp1.getFirstName(), emp1.getLastName(), "tech");
+				char[] generatedPassword = cs.generatePassword();
+				cs.showCredentials(emp1, email, generatedPassword);
+			}
+				break;
+			case 2: {
+				String email = cs.generateEmailAddress(emp1.getFirstName(), emp1.getLastName(), "admin");
+				char[] generatedPassword = cs.generatePassword();
+				cs.showCredentials(emp1, email, generatedPassword);
+			}
+				break;
+			case 3: {
+				String email = cs.generateEmailAddress(emp1.getFirstName(), emp1.getLastName(), "Human Resource");
+				char[] generatedPassword = cs.generatePassword();
+				cs.showCredentials(emp1, email, generatedPassword);
+			}
+				break;
+			case 4: {
+				String email = cs.generateEmailAddress(emp1.getFirstName(), emp1.getLastName(), "Legal");
+				char[] generatedPassword = cs.generatePassword();
+				cs.showCredentials(emp1, email, generatedPassword);
+			}
+				break;
+			}
+		} else {
+			System.out.println("Enter the Correct Option");
+		}
+		in.close();
+
+	}
+
+}
